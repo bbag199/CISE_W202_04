1b5d00f75a55f239034b884324564273bd9d0e38
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-09-18 02:17:09 +1200

    Updated color of Navbar
    I like this color, we can change >.<

--- frontend/src/app/pages/browse/page.tsx
+++ frontend/src/app/pages/browse/page.tsx
@@ -3,9 +3,11 @@ import React from "react";
 const page = () => {
   return (
     <div className="mt-16">
-      <div className="flex justify-center items-center space-x-4 xl:w-3/4 mx-auto"> 
-        <span className="font-bold text-xl flex-shrink-0 whitespace-nowrap">Browse articles</span>
-        
+      <div className="flex justify-center items-center space-x-4 xl:w-3/4 mx-auto">
+        <span className="font-bold text-xl flex-shrink-0 whitespace-nowrap">
+          Browse articles
+        </span>
+
         <input
           type="search"
           className="block w-full md:w-2/3 lg:w-1/2 xl:w-3/4 rounded-full border border-solid border-neutral-300 bg-white px-6 py-3 text-base font-normal leading-6 text-neutral-700 outline-none shadow-md transition duration-200 ease-in-out focus:z-[3] focus:border-blue-500 focus:ring focus:ring-blue-300 dark:border-neutral-600 dark:bg-neutral-800 dark:text-neutral-200 dark:placeholder:text-neutral-400 dark:focus:border-blue-500"
@@ -13,7 +15,7 @@ const page = () => {
           aria-label="Search"
           aria-describedby="button-addon2"
         />
-        
+
         <span
           className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-sm font-normal text-neutral-700 dark:text-neutral-200"
           id="basic-addon2"
@@ -33,24 +35,27 @@ const page = () => {
         </span>
       </div>
       <div className="mt-16">
-      <div className="px-4 md:px-8"> 
-        <table className="min-w-full table-auto border border-gray-300">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="border px-4 py-2 text-left font-bold">Title</th>
-              <th className="border px-4 py-2 text-left font-bold">Authors</th>
-              <th className="border px-4 py-2 text-left font-bold">Source</th>
-              <th className="border px-4 py-2 text-left font-bold">Year</th>
-              <th className="border px-4 py-2 text-left font-bold">DOI</th>
-              <th className="border px-4 py-2 text-left font-bold">Claim</th>
-              <th className="border px-4 py-2 text-left font-bold">Evidence</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
+        <div className="px-4 md:px-8">
+          <table className="min-w-full table-auto border border-blue-300">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border px-4 py-2 text-left font-bold">Title</th>
+                <th className="border px-4 py-2 text-left font-bold">
+                  Authors
+                </th>
+                <th className="border px-4 py-2 text-left font-bold">Source</th>
+                <th className="border px-4 py-2 text-left font-bold">Year</th>
+                <th className="border px-4 py-2 text-left font-bold">DOI</th>
+                <th className="border px-4 py-2 text-left font-bold">Claim</th>
+                <th className="border px-4 py-2 text-left font-bold">
+                  Evidence
+                </th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,35 +3,37 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-zinc-400 shadow-lg">
-      <div className="flex-1 text-3xl">
-        <a className="btn btn-ghost text-3xl">SPEED</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 text-3xl font-bold">
-          <li>
-            <Link  href="/home">Home</Link>
-          </li>
-          <li>
-            <details>
-              <summary >Menu</summary>
-              <ul className="bg-base-100 rounded-t-none p-2 text-lg">
-                <li>
-                  <Link href="/pages/browse">Browse</Link>
-                </li>
-                <li>
-                  <Link href="/pages/submit">Submit</Link>
-                </li>
-                <li>
-                  <Link href="/pages/review">Review</Link>
-                </li>
-                <li>
-                  <Link href="/pages/login">Login</Link>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+    <div className="pt-5">
+      <div className="navbar bg-blue-100 shadow-xl w-[97%] m-auto rounded-md">
+        <div className="flex-1 text-3xl">
+          <a className="btn btn-ghost text-3xl">SPEED</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 text-3xl font-bold">
+            <li>
+              <Link href="/home">Home</Link>
+            </li>
+            <li>
+              <details>
+                <summary>Menu</summary>
+                <ul className="bg-base-100 rounded-t-none p-2 text-lg">
+                  <li>
+                    <Link href="/pages/browse">Browse</Link>
+                  </li>
+                  <li>
+                    <Link href="/pages/submit">Submit</Link>
+                  </li>
+                  <li>
+                    <Link href="/pages/review">Review</Link>
+                  </li>
+                  <li>
+                    <Link href="/pages/login">Login</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
