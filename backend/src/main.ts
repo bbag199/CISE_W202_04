b5833357df1a46127faadb9ba7d71fea95128986
cam-arzo <camoarrow4586@gmail.com>
2024-09-18 13:45:28 +1200

    Add QOL changes, global gitignore

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+// Load .env file from the root folder
+dotenv.config({ path: join(__dirname, '..', '.env') });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.BACKEND_PORT;
+  await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
