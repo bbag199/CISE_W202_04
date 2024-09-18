4ce8abf75a3c7526abe0243b7fd7a7d3a7338ca2
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-09-19 10:57:58 +1200

    Initial connection to the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+  imports: [
+    ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+    controllers: [AppController],
+    providers: [AppService],
+    })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,24 @@
-import * as dotenv from 'dotenv';
-import { join } from 'path';
+// import * as dotenv from 'dotenv';
+// import { join } from 'path';
 
-// Load .env file from the root folder
-dotenv.config({ path: join(__dirname, '..', '.env') });
+// // Load .env file from the root folder
+// dotenv.config({ path: join(__dirname, '..', '.env') });
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const port = process.env.BACKEND_PORT;
+//   await app.listen(port, () => console.log(`Server running on port ${port}`));
+// }
+// bootstrap();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.BACKEND_PORT;
+  const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
