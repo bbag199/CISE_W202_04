7db516b6b33a3cb47f9453edf596f19f5be74d97
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-09-24 16:45:15 +1200

    Added the search function in the browse page

--- backend/src/api/article.controller.ts
+++ backend/src/api/article.controller.ts
@@ -16,6 +16,11 @@ export class ArticlesController {
   async findAll(): Promise<Article[]> {
     return this.articlesService.findAll();
   }
+  
+  @Get('search')
+  async searchByTitle(@Query('title') title: string): Promise<Article[]> {
+    return this.articlesService.searchByTitle(title);
+  }  
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Article> {
@@ -37,10 +42,6 @@ export class ArticlesController {
     return this.articlesService.delete(id);
   }
 
-  @Get('search')
-  async searchByTitle(@Query('title') title: string) {
-    return this.articlesService.searchByTitle(title);
-  }
 
   @Get('status/unmoderated')
   async findUnmoderatedArticles(): Promise<Article[]> {
