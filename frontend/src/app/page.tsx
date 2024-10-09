986acae7bd9ff0e39109ce4e8433cb1513b0d626
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-10-10 10:06:40 +1300

    Made the homepage load different whether logged in or not
    - if logged in, load dashboard
    - else, load log in page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,110 +1,23 @@
-"use client";
+'use client'
 
-import React, { useState, useEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-import { useAuth } from "../context/AuthContext";
-
-const Dashboard = () => {
-
+const Page = () => {
   const { isLoggedIn } = useAuth();
+  const router = useRouter();
 
-  const [awaitingModeration, setAwaitingModeration] = useState<number | null>(null);
-  const [awaitingAnalysis, setAwaitingAnalysis] = useState<number | null>(null);
-
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect( () => {
-    
-    setLoading(true);
-
-    const fetchAwaitingModeration = async () => {
-      try {
-        const response = await fetch('http://localhost:8082/articles/count/Unmoderated');
-        const data = await response.json();
-        setAwaitingModeration(data);
-      } catch (error) {
-        console.error('Error fetching unmoderated count: ', error);
-      }
+  // checks if the user is logged in or not
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/dashboard");
+    } else {
+      router.push("/pages/login");
     }
+  }, [isLoggedIn, router]); // runs the effect when the component loads 
 
-    const fetchAwaitingAnalysis = async () => {
-      try {
-        const response = await fetch('http://localhost:8082/articles/count/Moderated');
-        const data = await response.json();
-        setAwaitingAnalysis(data);
-      } catch (error) {
-        console.error('Error fetching moderated count:', error);
-      }
-    }
-
-    const fetchAllData = async () => {
-      setLoading(true);
-      fetchAwaitingModeration();
-      fetchAwaitingAnalysis();
-      setLoading(false);
-    }
-
-    fetchAllData();
-
-  }, []);
-
-  if (loading) {
-    return (
-    <div className="min-h-screen flex flex-col items-center pt-5"> {/* Added padding-top to avoid overlap with navbar */}
-    
-      <div className="bg-white p-6 rounded shadow-lg w-full max-w-4xl mx-auto">
-        <h1 className="text-xl font-bold mb-4">Dashboard</h1>
-        <p>Loading...</p>
-      </div>
-
-    </div>
-    )
-  }
-
-  if (!isLoggedIn) {
-    return <div>Not logged in?</div>;
-  }
-
-  // When logged in (as admin?), show all notifications
-
-  return (
-
-  <div className="min-h-screen flex flex-col items-center pt-5"> {/* Added padding-top to avoid overlap with navbar */}
-    
-    <div className="bg-white p-6 rounded shadow-lg w-full max-w-4xl mx-auto">
-      <h1 className="text-xl font-bold mb-4">Dashboard</h1>
-
-      { awaitingModeration ? (
-      <>
-        <p className="block mb-2">
-        You have {awaitingModeration} Article{awaitingModeration !== 1 && ("s")} awaiting Moderation.
-        </p>
-      </>
-      ) : (
-        <p className="block mb-2">
-        No articles are currently awaiting Moderation.
-        </p>
-      )}
-
-      { awaitingAnalysis ? (
-      <>
-        <p className="block mb-2">
-        You have {awaitingAnalysis} Article{awaitingAnalysis !== 1 && ("s")} awaiting Analysis.
-        </p>
-      </>
-      ) : (
-        <p className="block mb-2">
-        No articles are currently awaiting Analysis.
-        </p>
-      )}
-
-      
-
-    </div>
-  
-  </div>
-  )
-
+  return null;
 };
 
-export default Dashboard;
+export default Page;
