d3501c7ae8bf043c2003f8104e70f1e4dd6d8cf0
Cam <camoarrow4586@gmail.com>
2024-10-13 18:59:06 +1300

    Revert "Merge remote-tracking branch 'origin/testing' into testing"
    This reverts commit 2ae173df622644981136a45345870ce7e071de4b, reversing
    changes made to 137864636797999428dd2c3955aa6bd738094066.

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    transform: {
-      '^.+\\.(ts|tsx|js|jsx)?$': 'babel-jest', // Transform TypeScript and JSX
-    },
-    testMatch: ['**/__tests__/**/*.(test|spec).(js|ts|jsx|tsx)'],
-  };
-  
